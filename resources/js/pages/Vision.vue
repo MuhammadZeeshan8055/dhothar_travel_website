5e37edca4ec8f57fdf8bbba848450b9d9a5262cb
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-07 17:25:24 +0500

    Vision page created

---
+++ resources/js/pages/Vision.vue
@@ -0,0 +1,61 @@
+<template>
+     <!-- fade-up -->
+    <section class="header-page">
+        <div class="bounce-in animate4">
+            <h2 class="header-pagetitle">Vision</h2>
+        </div>
+    </section>
+
+
+
+    <!--start page-->
+    <section id="internalpage">
+
+        <!--start container-->
+        <div class="container clearfix">
+
+
+            <!--start content-->
+            <div class="grid_8 centersinglegrid">
+
+                <!--start post-->
+                <div class="singlepost orange">
+
+
+
+                    <h1 class="titlesinglepost">Vision</h1>
+
+                    <p>
+                        Our vision is to be the premier provider of transformative travel experiences worldwide. We
+                        aspire to set new standards of excellence in the travel industry by offering innovative and
+                        immersive journeys that captivate the imagination and enrich the soul. With a steadfast
+                        commitment to sustainability, authenticity, and cultural sensitivity, we seek to forge
+                        meaningful connections between travelers and destinations, fostering a deeper understanding and
+                        appreciation of the world's diverse cultures and natural wonders. Our vision is to inspire
+                        wanderlust, promote responsible travel practices, and empower individuals to embark on journeys
+                        that broaden horizons, deepen connections, and create lifelong memories.
+                    </p>
+
+
+
+                </div>
+                <!--end post-->
+
+            </div>
+            <!--end content-->
+
+        </div>
+        <!--end container-->
+
+    </section>
+    <!--end internal page-->
+
+    <div class="divider"><span></span></div>
+</template>
+
+<script>
+
+export default {
+    name: 'Vision',
+}
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Our_services from '../pages/Our_services.vue'
 import Mission from '../pages/Mission.vue'
+import Vision from '../pages/Vision.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/our_services', name: 'Our_services', component: Our_services },
-  { path: '/mission', name: 'Mission', component: Mission }
+  { path: '/mission', name: 'Mission', component: Mission },
+  { path: '/vision', name: 'Vision', component: Vision },
 ]
 
 const router = createRouter({
