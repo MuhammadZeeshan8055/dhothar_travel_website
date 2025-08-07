1b22ae0e5bc18d01b7460871f0d30360b23e3f95
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-07 17:55:04 +0500

    Introduction page created

---
+++ resources/js/pages/Introduction.vue
@@ -0,0 +1,59 @@
+<template>
+     <!-- fade-up -->
+    <section class="header-page">
+        <div class="bounce-in animate4">
+            <h2 class="header-pagetitle">Introduction</h2>
+        </div>
+    </section>
+
+
+
+    <!--start page-->
+    <section id="internalpage">
+
+        <!--start container-->
+        <div class="container clearfix">
+
+
+            <!--start content-->
+            <div class="grid_8 centersinglegrid">
+
+                <!--start post-->
+                <div class="singlepost orange">
+
+
+
+                    <h1 class="titlesinglepost">Introduction</h1>
+
+                    <p>
+                        At DIG Travel & Tourism, we believe that every journey begins with a dream. Our mission is to
+                        turn those dreams into unforgettable experiences by crafting personalized travel itineraries
+                        tailored to your desires and preferences.
+                        Whether you're seeking a relaxing beach getaway, an adventurous trek through the mountains, or a
+                        cultural immersion in bustling cities, we're here to make it happen. With our team of
+                        experienced travel experts and extensive network of partners worldwide, we ensure that every
+                        aspect of your trip is meticulously planned and executed to perfection. We have an edge on the
+                        on our competitors with our presence in Gulf and Europe.
+                    </p>
+
+
+
+                </div>
+                <!--end post-->
+
+            </div>
+            <!--end content-->
+
+        </div>
+        <!--end container-->
+
+    </section>
+    <!--end internal page-->
+</template>
+
+<script>
+
+export default {
+    name: 'Introduction',
+}
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,6 +5,7 @@ import Our_services from '../pages/Our_services.vue'
 import Mission from '../pages/Mission.vue'
 import Vision from '../pages/Vision.vue'
 import More_about_us from '../pages/More_about_us.vue'
+import Introduction from '../pages/Introduction.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
@@ -12,6 +13,7 @@ const routes = [
   { path: '/mission', name: 'Mission', component: Mission },
   { path: '/vision', name: 'Vision', component: Vision },
   { path: '/more_about_us', name: 'More_about_us', component: More_about_us },
+  { path: '/introduction', name: 'Introduction', component: Introduction },
 ]
 
 const router = createRouter({
