1a394d1017257319255432802c8d75049c74fc59
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-07 17:25:36 +0500

    More_about_us page created

---
+++ resources/js/pages/More_about_us.vue
@@ -0,0 +1,68 @@
+<template>
+     <!-- fade-up -->
+    <section class="header-page">
+        <div class="bounce-in animate4">
+            <h2 class="header-pagetitle">More About Us</h2>
+        </div>
+    </section>
+
+
+
+    <!--start page-->
+    <section id="internalpage">
+
+        <!--start container-->
+        <div class="container clearfix">
+
+
+            <!--start content-->
+            <div class="grid_8 centersinglegrid">
+
+                <!--start post-->
+                <div class="singlepost orange">
+
+
+
+                    <h1 class="titlesinglepost">More About Us</h1>
+
+                    <p>
+                        We recognize the intricacies and time-consuming nature of the visa application process. That's
+                        why we proudly offer comprehensive visa services for destinations worldwide, aiming to simplify
+                        the procedure and alleviate stress for our clients. Our seasoned team of visa experts stands
+                        ready to assist you at every turn, offering guidance on destination-specific visa requirements,
+                        aiding in the completion of application forms, and ensuring all necessary supporting documents
+                        are prepared. Whether your travel is for business or leisure, our visa services are designed to
+                        save you valuable time and eliminate unnecessary hassles, ensuring a seamless and stress-free
+                        travel experience. Reach out to us today to discover more about our visa services for
+                        destinations across the world."
+                        And here's the revised version of the additional information:
+                        <br>
+                        <br>
+                        We have extensive range of services catering to various countries' visa application processes.
+                        From Schengen visas to Eastern Europe, UK, USA, Australia, New Zealand, Russia and beyond, we
+                        offer comprehensive support to streamline your visa application experience."
+                    </p>
+
+
+
+                </div>
+                <!--end post-->
+
+            </div>
+            <!--end content-->
+
+        </div>
+        <!--end container-->
+
+    </section>
+    <!--end internal page-->
+
+    <div class="divider"><span></span></div>
+</template>
+
+<script>
+
+export default {
+    name: 'More_about_us',
+}
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,12 +4,14 @@ import Home from '../pages/Home.vue'
 import Our_services from '../pages/Our_services.vue'
 import Mission from '../pages/Mission.vue'
 import Vision from '../pages/Vision.vue'
+import More_about_us from '../pages/More_about_us.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/our_services', name: 'Our_services', component: Our_services },
   { path: '/mission', name: 'Mission', component: Mission },
   { path: '/vision', name: 'Vision', component: Vision },
+  { path: '/more_about_us', name: 'More_about_us', component: More_about_us },
 ]
 
 const router = createRouter({
