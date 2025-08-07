4776905be65597b96815f33ec951523ae5921127
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-07 18:00:41 +0500

    Visa file processing page created

---
+++ resources/js/pages/Visa_file_processing.vue
@@ -0,0 +1,379 @@
+<template>
+     <!-- fade-up -->
+    <section class="header-page">
+        <!-- bounce-in animate4 -->
+        <div class="">
+            <h2 class="header-pagetitle">Visit File Processing</h2>
+        </div>
+    </section>
+
+
+
+    <!--start page-->
+    <section id="internalpage">
+
+        <!--start container-->
+        <div class="container clearfix">
+
+
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/france.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/france.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>France<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/spain.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/spain.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Spain<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/uk.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/uk.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>UK<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/usa.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/usa.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>USA<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/australia.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/australia.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Australia<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/canada.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/canada.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Canada<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/belgium.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/belgium.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Belgium<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1"
+                        href="assets/img/country_images/nethderland.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/netherland.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Netherlands<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/poland.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/poland.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Poland<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/germany.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/germany.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Germany<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/italy.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/italy.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Italy<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/hungary.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/hungary.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Hungary<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/czech.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/czech.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Czech Republic<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/greece.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/greece.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Greece<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1"
+                        href="assets/img/country_images/switzerland.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/switzerland.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Switzerland<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/portugal.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/portugal.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Portugal<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/denmark.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/denmark.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Denmark<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/sweden.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/sweden.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Sweden<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/norway.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/norway.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Norway<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+            <div class="grid_3 singlemasonry single-isotope-filter filterthree">
+                <!--start image gallery-->
+                <div class="singleimagegallery blue">
+
+                    <a class="fancybox-thumbs" data-fancybox-group="gallery-1" href="assets/img/country_images/japan.jpg">
+                        <div class="btngallerypage rotate"></div>
+                    </a>
+
+                    <img alt="" class="imgsingleimagegallery" src="assets/img/country_images/japan.jpg" height="168px">
+
+                    <div class="titlesingleimagegallery">
+                        <p>Japan<span class="bulletgallery"></span></p>
+                    </div>
+
+                </div>
+                <!--end image gallery-->
+            </div>
+
+        </div>
+        <!--end container-->
+
+    </section>
+    <!--end internal page-->
+
+    <div class="divider"><span></span></div>
+</template>
+
+<script>
+
+export default {
+    name: 'Visa_file_processing',
+}
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -7,6 +7,7 @@ import Vision from '../pages/Vision.vue'
 import More_about_us from '../pages/More_about_us.vue'
 import Introduction from '../pages/Introduction.vue'
 import Visit_visa from '../pages/Visit_visa.vue'
+import Visa_file_processing from '../pages/Visa_file_processing.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
@@ -16,6 +17,7 @@ const routes = [
   { path: '/more_about_us', name: 'More_about_us', component: More_about_us },
   { path: '/introduction', name: 'Introduction', component: Introduction },
   { path: '/visit_visa', name: 'Visit_visa', component: Visit_visa },
+  { path: '/visa_file_processing', name: 'Visa_file_processing', component: Visa_file_processing },
 ]
 
 const router = createRouter({
