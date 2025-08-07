53d79ae2d0456ee49c714e5f91f84835dd37dd2f
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-07 18:06:43 +0500

    Default scroll behaviour

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -10,21 +10,22 @@ import Visit_visa from '../pages/Visit_visa.vue'
 import Visa_file_processing from '../pages/Visa_file_processing.vue'
 import Travel_insurance from '../pages/Travel_insurance.vue'
 
-const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/our_services', name: 'Our_services', component: Our_services },
-  { path: '/mission', name: 'Mission', component: Mission },
-  { path: '/vision', name: 'Vision', component: Vision },
-  { path: '/more_about_us', name: 'More_about_us', component: More_about_us },
-  { path: '/introduction', name: 'Introduction', component: Introduction },
-  { path: '/visit_visa', name: 'Visit_visa', component: Visit_visa },
-  { path: '/visa_file_processing', name: 'Visa_file_processing', component: Visa_file_processing },
-  { path: '/travel_insurance', name: 'Travel_insurance', component: Travel_insurance },
-]
-
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/our_services', name: 'Our_services', component: Our_services },
+    { path: '/mission', name: 'Mission', component: Mission },
+    { path: '/vision', name: 'Vision', component: Vision },
+    { path: '/more_about_us', name: 'More_about_us', component: More_about_us },
+    { path: '/introduction', name: 'Introduction', component: Introduction },
+    { path: '/visit_visa', name: 'Visit_visa', component: Visit_visa },
+    { path: '/visa_file_processing', name: 'Visa_file_processing', component: Visa_file_processing },
+    { path: '/travel_insurance', name: 'Travel_insurance', component: Travel_insurance },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  }
+});
 
-export default router
+export default router;
