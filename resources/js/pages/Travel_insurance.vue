15d65bb94194ba1c6cfb11c7e3e8a769da8ea42c
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-07 18:24:55 +0500

    Divider added in travel insurance page

--- resources/js/pages/Travel_insurance.vue
+++ resources/js/pages/Travel_insurance.vue
@@ -1,47 +1,57 @@
 <template>
-     <!-- fade-up -->
+    <!-- fade-up -->
     <section class="header-page">
         <!-- bounce-in animate4 -->
-	<div class=""><h2 class="header-pagetitle">Travel Insurance</h2></div>
-</section>
+        <div class="">
+            <h2 class="header-pagetitle">Travel Insurance</h2>
+        </div>
+    </section>
 
 
 
-<!--start page-->
-<section id="internalpage">
+    <!--start page-->
+    <section id="internalpage">
 
-	<!--start container-->
-    <div class="container clearfix">
-
-
-        <!--start content-->
-        <div class="grid_8 centersinglegrid">
-
-            <!--start post-->
-            <div class="singlepost orange">
+        <!--start container-->
+        <div class="container clearfix">
 
 
+            <!--start content-->
+            <div class="grid_8 centersinglegrid">
 
-                <h1 class="titlesinglepost">Travel Insurance</h1>
-
-                <p>
-                    Discover Our Exclusive Travel Insurance Services”
-                    Certainly! We offer comprehensive travel insurance services to provide you with peace of mind during your travels. Our insurance coverage includes protection against trip cancellations, medical emergencies, lost luggage, and other unforeseen events. Whether you’re traveling for business or pleasure, our travel insurance services are designed to safeguard you against unexpected disruptions and ensure a smooth and worry-free journey. Contact us today to learn more about our travel insurance options and how we can help protect your next trip.
-                </p>
+                <!--start post-->
+                <div class="singlepost orange">
 
 
+
+                    <h1 class="titlesinglepost">Travel Insurance</h1>
+
+                    <p>
+                        Discover Our Exclusive Travel Insurance Services”
+                        Certainly! We offer comprehensive travel insurance services to provide you with peace of mind
+                        during your travels. Our insurance coverage includes protection against trip cancellations,
+                        medical emergencies, lost luggage, and other unforeseen events. Whether you’re traveling for
+                        business or pleasure, our travel insurance services are designed to safeguard you against
+                        unexpected disruptions and ensure a smooth and worry-free journey. Contact us today to learn
+                        more about our travel insurance options and how we can help protect your next trip.
+                    </p>
+
+
+
+                </div>
+                <!--end post-->
 
             </div>
-            <!--end post-->
+            <!--end content-->
 
         </div>
-        <!--end content-->
+        <!--end container-->
 
-    </div>
-    <!--end container-->
+    </section>
+    <!--end internal page-->
 
-</section>
-<!--end internal page-->
+    <!--end homeclients-->
+    <div class="divider"><span></span></div>
 </template>
 
 <script>
