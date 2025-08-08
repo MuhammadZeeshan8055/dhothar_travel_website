461c101adc26c9bbc4cf95cde98461b4df795d10
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-08 18:30:57 +0500

    correct error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,9 +38,9 @@
         }
     </style>
 
-    <!-- Styles / Scripts via Vite -->
+    <!-- Laravel Vite -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/assets/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 </head>
 <body>
